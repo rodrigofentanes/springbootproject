38596cd26611eef4fae92a86fcb9255e9604dfb3
Rodrigo Fentanes <rodrigo.fentanes@hotmail.com>
2025-04-23 18:49:51 -0300

    TMDB Service and File writter

---
+++ src/main/java/com/domain/springbootproject/model/ResponseSearchMovieDTO.java
@@ -0,0 +1,98 @@
+package com.domain.springbootproject.model;
+
+import java.util.ArrayList;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.google.gson.Gson;
+
+public class ResponseSearchMovieDTO {
+  private static final ResponseSearchMovieDTO instance = new ResponseSearchMovieDTO();
+	public static ResponseSearchMovieDTO getInstance() { return instance; }
+  
+  private int page;
+  private ArrayList<Result> results;
+  private int total_pages;
+  private int total_results;
+
+  public int getPage() {
+    return page;
+  }
+
+  public ArrayList<Result> getResults() {
+    return results;
+  }
+
+  public int getTotal_pages() {
+    return total_pages;
+  }
+
+  public int getTotal_results() {
+    return total_results;
+  }
+
+  public class Result{
+    private boolean adult;
+    private String backdrop_path;
+    private ArrayList<Integer> genre_ids;
+    private int id;
+    private String original_language;
+    private String original_title;
+    private String overview;
+    private double popularity;
+    private String poster_path;
+    private String release_date;
+    private String title;
+    private boolean video;
+    private double vote_average;
+    private int vote_count;
+
+    public boolean isAdult() {
+      return adult;
+    }
+    public String getBackdrop_path() {
+      return backdrop_path;
+    }
+    public ArrayList<Integer> getGenre_ids() {
+      return genre_ids;
+    }
+    public int getId() {
+      return id;
+    }
+    public String getOriginal_language() {
+      return original_language;
+    }
+    public String getOriginal_title() {
+      return original_title;
+    }
+    public String getOverview() {
+      return overview;
+    }
+    public double getPopularity() {
+      return popularity;
+    }
+    public String getPoster_path() {
+      return poster_path;
+    }
+    public String getRelease_date() {
+      return release_date;
+    }
+    public String getTitle() {
+      return title;
+    }
+    public boolean isVideo() {
+      return video;
+    }
+    public double getVote_average() {
+      return vote_average;
+    }
+    public int getVote_count() {
+      return vote_count;
+    }
+  }
+
+  public ResponseSearchMovieDTO parse(String json) throws JsonMappingException, JsonProcessingException {
+    ResponseSearchMovieDTO data = new Gson().fromJson(json, ResponseSearchMovieDTO.class);
+    return (ResponseSearchMovieDTO) data;
+  }
+}
---
+++ src/main/java/com/domain/springbootproject/services/TmdbServices.java
@@ -0,0 +1,25 @@
+package com.domain.springbootproject.services;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class TmdbServices {
+  private static final TmdbServices instance = new TmdbServices();
+	public static TmdbServices getInstance() { return instance; }
+
+  public String serachMovieByName(String name) throws IOException, InterruptedException {
+    HttpClient client = HttpClient.newHttpClient();
+    HttpRequest request = HttpRequest.newBuilder()
+    .uri(URI.create("https://api.themoviedb.org/3/search/movie?query=" + name.replace(" ", "+") + "&include_adult=true&language=en-US"))
+    .setHeader("accept", "application/json")
+    .setHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNGViNjk5N2E4YjM3NjhhZDc5M2ZkZGNmOTYxNTE0NCIsIm5iZiI6MTYwNTEzNjMzNi4xODksInN1YiI6IjVmYWM2ZmQwZmNiOGNjMDA0MGY4NzczNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.j_J0q5nvtJMlt5wB6prMvYY305nFyPjBkSIlNskp3X8")
+    .build();
+
+    HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+    return response.body();
+  }
+}
